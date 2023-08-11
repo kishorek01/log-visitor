7b3302c19b3f3331a7931de5a8acc7003273d56a
kishorekumargit <nkishorekumar10@gmail.com>
2023-08-11 11:28:40 +0530

    changes in logger

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const requestIP = require('request-ip');
+const log = require('log-to-file');
+
 app.get('/', (req, res) => {
     const ipAddress = requestIP.getClientIp(req);
+    log(ipAddress);
     console.log(ipAddress);
     // res.send(ipAddresses);
     res.send('Hello World!')
